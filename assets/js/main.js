ed79abf859be68cb057353e7d84566322cdddeb8
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2020-03-20 04:43:02 +0100

    Implémentation de la rubrique Ailleurs

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,9 +17,10 @@ $(document).ready(function () {
         });
     });
 
-    function addPub(data) {
+
+    function addPub(data, section = 'home') {
         let d = moment(data.datePub).format("dddd, Do MMMM YYYY [à] HH:mm");
-        $("#home").prepend(
+        $(`#${section}`).prepend(
             `<div class="publication">
             <h1 class="title">${data.titre}</h1>
             <div class="image">
@@ -30,7 +31,7 @@ $(document).ready(function () {
                     <i class="fa fa-clock-o"></i>
                     <time datetime="2020-03-16"> publié le ${d}</time>
                 </div>
-                <p>${data.contenu || "Contenu"}</p>
+                <p>${data.description || "Contenu"}</p>
                 <div class="pub-footer">
                     <button class="button"><i class="fa fa-share-alt"></i><span>Partager</span></button>
                     <p>Source : <a href="${data.sourceLien}">${data.sourceNom}</a></p>
@@ -39,4 +40,55 @@ $(document).ready(function () {
         </div>`
         );
     }
+
+
+    // Recuperation des flux rss
+    const RSS_URL = `https://www.mediapart.fr/articles/feed`;
+
+    /**
+     * Place une information provenant d'une url dans l'acceuil
+     * @param {string} RSS_URL
+     */
+    function GetInfoCovid19(RSS_URL) {
+
+        const covidRegex = /(covid-19|coronavirus)/gmi;
+
+        $.ajax(RSS_URL, {
+            accepts: {
+                xml: "application/rss+xml"
+            },
+            dataType: "xml",
+            success: function (data) {
+                // On retient le nom de la source
+                const source = $(data).find("channel").find("title").get()[0].textContent;
+
+                $(data)
+                    .find("item")
+                    .each(function () {
+                        const el = $(this);
+                        const title = el.find("title").text();
+                        const description = el.find("description").text();
+                        const subjectCovidTabs = [...title.matchAll(covidRegex), ...description.matchAll(covidRegex)];
+
+                        if (subjectCovidTabs && subjectCovidTabs.length > 0) {
+
+                            const imageUrl = el.get()[0].querySelector('content') ? el.get()[0].querySelector('content').getAttribute('url') : `https://fcw.com/-/media/GIG/EDIT_SHARED/Health-IT/covid19.jpg`;
+                            const date = new Date(el.find("pubDate").text());
+
+                            const params = {
+                                titre: title,
+                                image: imageUrl,
+                                datePub: date,
+                                description: description,
+                                sourceNom: source,
+                                sourceLien: el.find("link").text()
+                            };
+                            addPub(params, "ailleurs");
+                        }
+                    })
+            }
+        })
+    }
+    GetInfoCovid19(RSS_URL);
+
 });
