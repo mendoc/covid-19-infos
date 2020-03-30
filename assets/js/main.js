93e1391ec53410cb3ef7ff86eb44ceadad1a193d
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2020-03-30 05:10:15 +0100

    Ajout de la section Cartes

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@
             videos.innerHTML = '';
             querySnapshot.forEach(function (doc) {
                 let pub = doc.data();
-                let item = '<div class="col mb-4"> <div class="card"> <figure class="snip1253"> <div class="image"><img src="' + pub.image + '" alt="sample52"/></div> <figcaption> <div class="date"><span class="day">28</span><span class="month">Oct</span></div> <h3>' + pub.titre + '</h3> <p>' + pub.description + '</p> </figcaption> <footer> <a class="font-italic" href="' + pub.sourceLien + '">Source : ' + pub.sourceNom + '</a></footer></figure></div></div>';
+                let item = '<div class="col mb-4"> <div class="card"> <figure class="snip1253"> <div class="image"><img src="' + pub.image + '" alt="sample52"/></div> <figcaption> <div class="date"><span class="day">28</span><span class="month">Mars</span></div> <h3>' + pub.titre + '</h3> <p>' + pub.description + '</p> </figcaption> <footer> <a class="font-italic" href="' + pub.sourceLien + '">Source : ' + pub.sourceNom + '</a></footer></figure></div></div>';
                 if (pub.type === 'VIDEO')
                     videos.innerHTML += item;
                 else
